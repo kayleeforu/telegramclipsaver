5a1ff9def3cf63a9b86d6dce9245a43a606114fc
kayleeforu <ak.kayleeforu@gmail.com>
2026-03-12 19:00:38 +0200

    YouTube download support

--- savevid.py
+++ savevid.py
@@ -5,11 +5,16 @@ def downloadVideo(url):
         count = int(f.read())
         f.seek(0)
         f.write(str(count + 1))
+    
     URL = [url]
-    filepath = f"downloadedVideos/video{count}.mp4"
+    filepath = f"downloadedVideos/video{count}.%(ext)s"
+    
     with YoutubeDL({
         "quiet": True,
-        "outtmpl": filepath
+        "outtmpl": filepath,
+        "format": "bestvideo+bestaudio/best",
+        "merge_output_format": "mp4",
+        "js_runtimes": ["node"],
         }) as ydl:
         try:
             ydl.download(URL)
