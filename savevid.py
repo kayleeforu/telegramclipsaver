221cd60f702b6612f5931050f4ad88760b878ce7
kayleeforu <ak.kayleeforu@gmail.com>
2026-03-12 19:26:29 +0200

    Changes to file extension save

--- savevid.py
+++ savevid.py
@@ -1,20 +1,23 @@
 from yt_dlp import YoutubeDL
 
 def downloadVideo(url):
-    URL = [url]
-    filepath = "downloadedVideos/video%(id)s.%(ext)s"
-    
-    with YoutubeDL({
+    ydl_opts = {
         "quiet": True,
-        "outtmpl": filepath,
+        "outtmpl": "downloadedVideos/video%(id)s.%(ext)s",
         "format": "bestvideo+bestaudio/best",
         "merge_output_format": "mp4",
-        "js_runtimes": {
-            "node": {}
-        }
-        }) as ydl:
-        try:
-            ydl.download(URL)
-        except:
-            return None
-    return filepath
+        "js_runtimes": {"node": {}}
+    }
+
+    try:
+        with YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=True)
+            filepath = ydl.prepare_filename(info)
+
+            if filepath.endswith(".webm") or filepath.endswith(".mkv"):
+                filepath = filepath.rsplit(".", 1)[0] + ".mp4"
+
+            return filepath
+
+    except Exception:
+        return None
