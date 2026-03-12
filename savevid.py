330e0813107ecfd0921ea479ba3471a7057fd9cd
kayleeforu <ak.kayleeforu@gmail.com>
2026-03-12 20:18:06 +0200

    Trying bypass bot detection

--- savevid.py
+++ savevid.py
@@ -4,14 +4,19 @@ def downloadVideo(url):
     ydl_opts = {
         "quiet": True,
         "outtmpl": "downloadedVideos/video%(id)s.%(ext)s",
-        "format": "bestvideo+bestaudio/best",
+        "format": "bv*[height<=720]+ba/b",
         "merge_output_format": "mp4",
-        "http_headers": {"User-Agent": "Mozilla/5.0"},
+        "http_headers": {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120 Safari/537.36"
+        },
         "extractor_args": {
             "youtube": {
                 "player_client": ["android", "ios", "web"]
             }
-        }
+        },
+        "retries": 10,
+        "fragment_retries": 10,
+        "source_address": "0.0.0.0",
     }
     try:
         with YoutubeDL(ydl_opts) as ydl:
