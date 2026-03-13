27046c458946231fa99c73c0022169efc171d57d
kayleeforu <ak.kayleeforu@gmail.com>
2026-03-13 17:44:50 +0200

    Trying to fasten the download speed

--- savevid.py
+++ savevid.py
@@ -4,7 +4,7 @@ def downloadVideo(url):
     ydl_opts = {
         "quiet": True,
         "outtmpl": "downloadedVideos/video%(id)s.%(ext)s",
-        "format": "bv*[height<=720]+ba/b",
+        "format": "bv*[height<=1080]+ba/b",
         "merge_output_format": "mp4",
         "cookiefile": "/home/ubuntu/bot/cookies.txt",
         "js_runtimes": {"node": {}},
@@ -13,9 +13,10 @@ def downloadVideo(url):
                 "player_client": ["web"],
             }
         },
-        "retries": 10,
         "fragment_retries": 10,
         "remote_components": ["ejs:github"],
+        "concurrent_fragment_downloads": 4,
+        "buffersize": 1024,
     }
     try:
         with YoutubeDL(ydl_opts) as ydl:
