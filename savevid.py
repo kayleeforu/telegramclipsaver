688b99631f4bc7548de787f6076eeba897ca8b3b
kayleeforu <ak.kayleeforu@gmail.com>
2026-03-13 20:13:02 +0200

    Format changes

--- savevid.py
+++ savevid.py
@@ -4,7 +4,7 @@ def downloadVideo(url):
     ydl_opts = {
         "quiet": True,
         "outtmpl": "downloadedVideos/video%(id)s.%(ext)s",
-        "format": "bestvideo[height<=1080][ext=mp4]+bestaudio[ext=m4a]/bestvideo[height<=1080]+bestaudio/best",
+        "format": "95/94/93/18",
         "merge_output_format": "mp4",
         "cookiefile": "/home/ubuntu/bot/cookies.txt",
         "js_runtimes": {"node": {}},
