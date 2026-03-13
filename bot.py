09014922f0816b689e0c7fe3928a7e039f2f9ef3
kayleeforu <ak.kayleeforu@gmail.com>
2026-03-13 21:17:20 +0200

    Mention final fix

--- bot.py
+++ bot.py
@@ -22,9 +22,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def video(update: Update, context: ContextTypes.DEFAULT_TYPE):
     link = update.message.text
     isGroupChat = update.effective_chat.type in ["group", "supergroup"]
-    requestedBy = update.effective_sender.username if isGroupChat else None
-    if requestedBy is not None:
-        requestedBy = "@".join(requestedBy)
+    requestedBy = "@" + update.effective_sender.username if isGroupChat else None
     requestedMessage = update.effective_message.id if isGroupChat else None
 
     filepath = downloadVideo(link)
---
+++ test.py
@@ -0,0 +1,3 @@
+string = "kayleeforu"
+string = "@" + string
+print(string)
