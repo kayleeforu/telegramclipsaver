e780c0c79e3aa2558c8bb193eda79dd20e2bcffb
kayleeforu <ak.kayleeforu@gmail.com>
2026-03-13 22:44:08 +0200

    Tiktok regex fix

--- bot.py
+++ bot.py
@@ -57,7 +57,7 @@ async def video(update: Update, context: ContextTypes.DEFAULT_TYPE):
         f.write(str(count))
 
 patterns = [
-    r"(https://)?v.\.tiktok\.com/.{9}/",
+    r"(https://)?v.\.tiktok\.com/.*/",
     r"(https://(www\.)?)?tiktok.com/@(.*)/(\d{19})\?.*",
     r"(https://(www\.)?)?youtube\.com/watch(.*)",
     r"(https://(www\.)?)?youtu\.be/.*",
