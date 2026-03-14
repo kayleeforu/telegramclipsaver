4f6e5d6e14edc5ae1dbbf08deec991d39f2d1b90
kayleeforu <ak.kayleeforu@gmail.com>
2026-03-14 14:12:10 +0200

    clearCount.py fix

--- clearCount.py
+++ clearCount.py
@@ -2,5 +2,5 @@ with open("downloadedCount.txt", "r+") as f:
     count = f.read()
     print(f"The Downloaded Count was: {count}.\n")
     f.seek(0)
+    f.truncate()
     f.write("0")
-    f.close()
