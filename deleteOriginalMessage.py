183338c16113254caad6e684ce05279c41db281d
kayleeforu <ak.kayleeforu@gmail.com>
2026-03-15 16:42:54 +0200

    Bot perms test

--- deleteOriginalMessage.py
+++ deleteOriginalMessage.py
@@ -8,6 +8,6 @@ async def deleteOriginalMessage(update, context, requestedMessage, requestedBy):
                     await context.bot.delete_message(update.effective_chat.id, requestedMessage)
                 except:
                     unableToDeleteMessage = await context.bot.send_message(update.effective_chat.id, text = "I can't delete the original message with a link.\n" \
-                    "If you want me to delete them, give me the right to delete the messages.")
+                    "If you want me to delete them, give me the right to delete the messages." + update.effective_message.text)
                     await asyncio.sleep(5)
                     await context.bot.delete_message(update.effective_chat.id, unableToDeleteMessage.message_id)
