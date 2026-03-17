745b78de9dd71ad7b72387ef03b0aeacc1b4b192
kayleeforu <ak.kayleeforu@gmail.com>
2026-03-17 09:02:28 +0200

    thumbnail debugging v3

--- getThumbnail.py
+++ getThumbnail.py
@@ -1,6 +1,7 @@
 import cv2
 
 async def getThumbnail(filepath):
+    print("\n\nTHUMBNAIL\n\n")
     framepath = filepath.replace(".mp4", "_frame.jpg")
     vid = cv2.VideoCapture("downloadedVideos/video222.mp4")
     success, image = vid.read()
