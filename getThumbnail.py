268b68c911911b3692b21fb77cdcd610c82021ab
kayleeforu <ak.kayleeforu@gmail.com>
2026-03-17 10:42:05 +0200

    changes to the file preview

--- getThumbnail.py
+++ getThumbnail.py
@@ -4,7 +4,8 @@ async def getThumbnail(filepath):
     print("\n\nTHUMBNAIL\n\n")
     framepath = filepath.replace(".mp4", "_frame.jpg")
     vid = cv2.VideoCapture(filepath)
-    success, image = vid.read()
+    for i in range(60):
+        success, image = vid.read()
     if success:
         cv2.imwrite(framepath, image)
     vid.release()
