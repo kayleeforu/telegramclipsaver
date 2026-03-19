2a62b66da3fa2635992af340d85f6b00ade16003
kayleeforu <ak.kayleeforu@gmail.com>
2026-03-19 12:51:59 +0200

    Fixing not downloading video for some of them from youtube

--- utilities/savevid.py
+++ utilities/savevid.py
@@ -6,7 +6,7 @@ from utilities.getVideoInfo import getVideoInfo
 def duration_filter(info):
     duration = info.get("duration")
     if duration and duration > 1800:
-        return "Video is too long (max 1800 minutes)"
+        return "Video is too long (max 1800 seconds)"
 
 async def downloadVideo(url):
     ydl_opts = {
@@ -16,14 +16,12 @@ async def downloadVideo(url):
         "format": "bestvideo[vcodec^=avc][height<=1080]+bestaudio/bestvideo[height<=1080]+bestaudio/best",
         "merge_output_format": "mp4",
         "cookiefile": "/home/kaylee/telegramclipsaver/cookies.txt",
-        "js_runtimes": {"node": {}},
         "extractor_args": {
             "youtube": {
-                "player_client": ["web", "web_safari"],
+                "player_client": ["ios", "tv_embedded", "web"],
             }
         },
         "fragment_retries": 10,
-        "remote_components": ["ejs:github"],
         "concurrent_fragment_downloads": 2,
         "buffersize": 8192,
         "postprocessors": [{
@@ -38,17 +36,14 @@ async def downloadVideo(url):
         with YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(url, download=True)
             filepath = ydl.prepare_filename(info)
-            if filepath.endswith(".webm") or filepath.endswith(".mkv") or filepath.endswith(".gif"):
+            if filepath.endswith((".webm", ".mkv", ".gif")):
                 filepath = filepath.rsplit(".", 1)[0] + ".mp4"
-            
-            thumbnailpath, height, width = await getVideoInfo(str(filepath))
 
+            thumbnailpath, height, width = await getVideoInfo(str(filepath))
             probe = ffmpeg.probe(filepath)
             audio_streams = [s for s in probe['streams'] if s['codec_type'] == 'audio']
-            if audio_streams == []:
-                return filepath, False, thumbnailpath, height, width
+            return str(filepath), bool(audio_streams), thumbnailpath, height, width
 
-            return str(filepath), True, thumbnailpath, height, width
     except DownloadError as e:
         if "too long" in str(e).lower():
             return "too_long", None, None, None, None
