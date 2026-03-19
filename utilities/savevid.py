cbd5977ad6c42d37598dd797192b341e2401c3f8
kayleeforu <ak.kayleeforu@gmail.com>
2026-03-19 12:54:36 +0200

    backing up the changes

--- utilities/savevid.py
+++ utilities/savevid.py
@@ -16,12 +16,14 @@ async def downloadVideo(url):
         "format": "bestvideo[vcodec^=avc][height<=1080]+bestaudio/bestvideo[height<=1080]+bestaudio/best",
         "merge_output_format": "mp4",
         "cookiefile": "/home/kaylee/telegramclipsaver/cookies.txt",
+        "js_runtimes": {"node": {}},
         "extractor_args": {
             "youtube": {
-                "player_client": ["ios", "tv_embedded", "web"],
+                "player_client": ["web", "web_safari"],
             }
         },
         "fragment_retries": 10,
+        "remote_components": ["ejs:github"],
         "concurrent_fragment_downloads": 2,
         "buffersize": 8192,
         "postprocessors": [{
@@ -36,14 +38,17 @@ async def downloadVideo(url):
         with YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(url, download=True)
             filepath = ydl.prepare_filename(info)
-            if filepath.endswith((".webm", ".mkv", ".gif")):
+            if filepath.endswith(".webm") or filepath.endswith(".mkv") or filepath.endswith(".gif"):
                 filepath = filepath.rsplit(".", 1)[0] + ".mp4"
-
+            
             thumbnailpath, height, width = await getVideoInfo(str(filepath))
+
             probe = ffmpeg.probe(filepath)
             audio_streams = [s for s in probe['streams'] if s['codec_type'] == 'audio']
-            return str(filepath), bool(audio_streams), thumbnailpath, height, width
+            if audio_streams == []:
+                return filepath, False, thumbnailpath, height, width
 
+            return str(filepath), True, thumbnailpath, height, width
     except DownloadError as e:
         if "too long" in str(e).lower():
             return "too_long", None, None, None, None
