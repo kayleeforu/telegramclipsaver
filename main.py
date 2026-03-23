3905b1f998526069a3236b98c01377b4ce3da946
kayleeforu <ak.kayleeforu@gmail.com>
2026-03-23 15:52:04 +0200

    Bot api url add to main.py

--- main.py
+++ main.py
@@ -25,16 +25,18 @@ combinedVideos = "|".join(f"({p})" for p in videoPost)
 
 if __name__ == '__main__':
     TOKEN = os.environ.get("BOT_TOKEN")
+    BOT_API_URL = os.getenv("BOT_API_URL", "http://telegram-bot-api:8081/bot")
+    BOT_API_FILE_URL = os.getenv("BOT_API_FILE_URL", "http://telegram-bot-api:8081/file/bot")
 
     application = ApplicationBuilder() \
-    .token(TOKEN) \
-    .base_url("http://localhost:8081/bot") \
-    .base_file_url("http://localhost:8081/file/bot") \
-    .concurrent_updates(True)\
-    .read_timeout(120) \
-    .write_timeout(120) \
-    .connect_timeout(120) \
-    .build()
+        .token(TOKEN) \
+        .base_url(BOT_API_URL) \
+        .base_file_url(BOT_API_FILE_URL) \
+        .concurrent_updates(True) \
+        .read_timeout(120) \
+        .write_timeout(120) \
+        .connect_timeout(120) \
+        .build()
 
     # Commands
     startHandler = CommandHandler("start", start)
