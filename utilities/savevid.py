772d4e8030e8889557b6cc4950c6b216f14ef573
kayleeforu <ak.kayleeforu@gmail.com>
2026-04-05 14:56:14 +0300

    removed impersonate

--- utilities/savevid.py
+++ utilities/savevid.py
@@ -52,7 +52,6 @@ def downloadVideo(url):
         "merge_output_format": "mp4",
         "cookiefile": "cookies.txt",
         "writethumbnail": True,
-        "impersonate": "chrome120",
         "http_headers": {
             "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1",
         },
