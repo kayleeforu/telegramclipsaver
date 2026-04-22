81b8b36a5384b5669e9363300d62f2ac1a6be521
kayleeforu <ak.kayleeforu@gmail.com>
2026-04-22 13:13:31 +0300

    debugging link, because yt-dlp doesnt see the link

--- handlers/linkProcessing.py
+++ handlers/linkProcessing.py
@@ -13,6 +13,7 @@ database = db.database()
 
 async def processLink(update: Update, context: ContextTypes.DEFAULT_TYPE, link):
     isTiktok = "tiktok" in link
+    logging.info(f"[processLink] link: {link}")
 
     def runDownload():
         return downloadVideo(link)
