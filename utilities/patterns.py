5d7b3109ccb4995303f6743e8038ff42e116ffef
kayleeforu <ak.kayleeforu@gmail.com>
2026-05-03 22:36:40 +0300

    tiktok back to yt-dlp

--- utilities/patterns.py
+++ utilities/patterns.py
@@ -4,6 +4,9 @@ videoPost = [
     r"((https://(www\.)?)?youtube\.com/watch(\S*))",
     r"((https://(www\.)?)?youtu\.be/\S*)",
     r"((https://(www\.)?)?youtube\.com/shorts/\S*)",
+    r"((https://)?v.\.tiktok\.com/\S*)",
+    r"((https://(www\.)?)?tiktok.com/@(.*)/(\d{19})\?\S*)",
+    r"((https://(www\.)?)?tiktok.com/\S*)",
 ]
 combinedVideos = "|".join(f"({p})" for p in videoPost)
 
@@ -12,9 +15,6 @@ galleryDl = [
     r"((https://(www\.)?)?instagram\.com/reel/\S*)",
     r"((https://(www\.)?)?pin\..{2}/\S*)",
     r"((https://(www\.)?)?pinterest\.com/pin/\S*)",
-    r"((https://)?v.\.tiktok\.com/\S*)",
-    r"((https://(www\.)?)?tiktok.com/@(.*)/(\d{19})\?\S*)",
-    r"((https://(www\.)?)?tiktok.com/\S*)",
 ]
 combinedGalleryDl = "|".join(f"({p})" for p in galleryDl)
 
